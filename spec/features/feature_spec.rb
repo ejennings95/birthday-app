69efad3ca3e2604bd9e509e828beb5d67389df29
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-24 10:51:18 +0000

    asks you to input your birthday and has a form to fill in. passes tests

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -16,13 +16,29 @@ end
 feature 'The homepage should have a text box to fill in your name' do
   scenario 'under the question of what is your name there should be a text box' do
     visit('/')
-    find('form').visible?
+    find('form1').visible?
   end
 end
 
-feature 'The homepage asks for your name' do
+feature 'The homepage asks for your Birthday' do
   scenario 'the homepage will have the question When is your birthday?' do
     visit('/')
     expect(page).to have_content 'When is your birthday?'
+  end
+end
+
+feature 'The homepage asks for your birth day and month' do
+  scenario 'the homepage will have the question When is your birthday?' do
+    visit('/')
+    expect(page).to have_content 'Please input your birthday:'
+    expect(page).to have_content 'Day:'
+    expect(page).to have_content 'Month:'
+end
+end
+
+feature 'The homepage should have a text box to fill in your birth day and month' do
+  scenario 'under the question of when is your Birthday there should be a text box' do
+    visit('/')
+    find('form2').visible?
   end
 end
