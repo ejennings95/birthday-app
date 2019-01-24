cbc97468e45de70253a6ac15d9f85dce859977a3
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-24 10:20:52 +0000

    asks what your name is and passes the test

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,14 @@
 feature 'You should see a welcome message at the top of the page' do
   scenario 'Welcome to the birthday app to be on the homepage' do
     visit('/')
-    expect(page).to have_content 'Welcome to the birthday app!'
+    expect(page).to have_content 'Welcome to the birthday app!!'
+  end
+end
+
+
+feature 'The homepage asks for your name' do
+  scenario 'the homepage will have the question What is your name?' do
+    visit('/')
+    expect(page).to have_content 'What is your name?'
   end
 end
